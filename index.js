ce9eca1e18e84d62731d258b6f810618d8613755
Simon Walz <contact@simonwalz.de>
2018-07-08 22:55:45 +0200

    refactored app

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 var W1Temp = require("w1temp");
 
 exports.init = function(node, app_config, main, host_info) {
+	var timer = [];
+
+	var get_temperature_interval = function(node, sensor, sid) {
+		var sid
+		console.log("set Interval", sensor.file);
+		var t = setInterval(function() {
+			console.log("interval", sid);
+			sensor.getTemperatureAsync().then(function(temp){
+				console.log('Actual temp:', temp, 'C', sid);
+				node.publish(undefined, temp);
+			}).catch(function(err) {
+				throw err;
+			});
+		}, 5000);
+		timer.push(t);
+	};
 
 	var map = node.map(app_config.map, null, true, undefined,
 		function(n, metadata, c) {
+			var sid = c.map;
+
+			// get instance of temperature sensor
+			W1Temp.getSensor(sid, false).then(function (sensor) {
+				// print actual temperature
+
+				get_temperature_interval(n, sensor, sid);
+			}).catch(function(err) {
+				throw err;
+			});
+
 			metadata.type = "temperature.data";
 			metadata.unit = "C";
 			metadata.unit_long = "Celsius";
@@ -19,22 +46,10 @@ exports.init = function(node, app_config, main, host_info) {
 		sensorsUids.forEach(function(sid) {
 			var n = map.node(sid);
 			if (!n) return;
-
-			// get instance of temperature sensor
-			W1Temp.getSensor(sid).then(function (sensor) {
-				// print actual temperature
-				var temp = sensor.getTemperature();
-				console.log('Actual temp:', temp, 'C');
-				n.publish(undefined, temp);
-
-				// print actual temperature on changed
-				sensor.on('change', function (temp) {
-					console.log('Temp changed:',temp,'C');
-					n.publish(undefined, temp);
-				});
-			});
 		});
+	}).catch(function(err) {
+		throw err;
 	});
-	return map;
+	return [map, timer];
 }
 
